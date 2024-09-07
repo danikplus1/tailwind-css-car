73978d605e2b76d6b5ff2935146327fbc156d6be
danikplus1 <danik.solovei@gmail.com>
2024-09-07 13:27:46 +0200

    add html structure, config some elements, add fonts, add tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     extend: {
       fontFamily: {
         bebas: ["Bebas Neue", "sans-serif"],
+        "roboto-flex": ["Roboto Flex", "sans-serif"],
+        "roboto-condensed": ["Roboto Condensed", "sans-serif"],
+      },
+      colors: {
+        primary: "#f7f7f7",
+        secondary: "#282828",
+        active: "#0147FF",
       },
     },
   },
